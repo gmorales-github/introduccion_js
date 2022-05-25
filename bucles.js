db8368ffe1f8aaeba28e90dc3d6a768518aca9d1
gmorales-github <gustavom.morales@gmail.com>
2022-05-25 19:52:47 -0300

    Clase uno

---
+++ bucles.js
@@ -0,0 +1,7 @@
+// Estruturas de control
+// El bloque for
+
+// Bloque for sencillo
+for (let index = 1; index < 5; index++) {
+    console.log("Hola");    
+}
---
+++ variables.js
@@ -0,0 +1,16 @@
+// constantes y varibales en js
+// Declaracion de constantes con la palabra clave (keyword) "const"
+const animal = 'koala';
+const pi = 3.14;
+const anio_cero = 14081995;
+
+// Declaracion de variables con la palabras claves (keywords) "var" y "let"
+
+let dinosaurio = 'T-rex';
+var pais = 'Argentina';
+let cantidad = 20; 
+
+// muestro los datos por consola
+console.log(pi);
+console.log(dinosaurio);
+console.log("El valor del la variable cantidad: " + cantidad);
